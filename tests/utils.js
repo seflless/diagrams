c338c8a0f3db70653458175969b854fc8b074002
Francois Laberge <francoislaberge@Jibos-MacBook-Pro-2.local>
2017-08-27 10:21:30 -0400

    Trying with absolute directory

--- tests/utils.js
+++ tests/utils.js
@@ -1,9 +1,10 @@
 let fs = require('fs-extra');
+let path = require('path');
 
 let tmpDir;
 module.exports = {
     init(cb){
-        tmpDir = 'tests/tmp';
+        tmpDir = path.resolve(__dirname, '../tests/tmp');
         fs.emptyDir(tmpDir, (err) => {
             cb(err);
         });
